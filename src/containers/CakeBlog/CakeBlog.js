a1cb69f21c2e127c6cd3a09f31fcd0165e29975c
Christopher Gorman <mail@chrisgorman.co.uk>
2018-12-03 11:15:33 +0000

    S2 - As a cake lover, I can submit cakes that I like so everyone can drool at my tasty

--- src/containers/CakeBlog/CakeBlog.js
+++ src/containers/CakeBlog/CakeBlog.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import { Route, NavLink, Switch } from 'react-router-dom';
+import classes from './CakeBlog.css';
 import Cakes from './Cakes/Cakes';
+import UploadCake from './UploadCake/UploadCake';
 
 class CakeBlog extends Component {
-	render () {
+    render () {
         return (
-            <Cakes />
+            <div>
+                <header>
+                    <nav className={classes.CakeBlog}>
+                        <ul>
+                            <li><NavLink
+                                to="/"
+                                exact
+                            >Cakes</NavLink></li>
+                            <li><NavLink
+                            	to='/upload-cake'
+                            >Upload Cake</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Switch>
+                	<Route path="/upload-cake" component={UploadCake} />
+                    <Route path="/" component={Cakes} />
+                </Switch>
+            </div>
         );
     }
 }
